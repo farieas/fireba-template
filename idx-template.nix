d185aee447ba74dfcbc152db37d969708b47c14d
farhankdi <43706147+farieas@users.noreply.github.com>
2025-10-15 09:15:52 +0530

    Update idx-template.nix

--- idx-template.nix
+++ idx-template.nix
@@ -1,25 +1,15 @@
-{ pkgs, sample ? "none", template ? "app", platforms ? "android,ios,web", statemanagement ? "bloc", ... }:
-{
-  channel = "unstable";
+{ pkgs, ... }: {
   packages = [
-    pkgs.git
-    pkgs.curl
-    pkgs.gnutar
-    pkgs.xz
-    pkgs.flutter
-    pkgs.jdk21
-    pkgs.busybox
+   
   ];
-  
   bootstrap = ''
-    
-    echo "Creating Flutter project using Firebase Studio Flutter version..."
-    flutter create "$out" --platforms="${platforms}" --template="${template}"
-
-    mkdir -p "$out"/.idx
-    cp ${./dev.nix} "$out"/.idx/dev.nix
-    install --mode u+rw ${./dev.nix} "$out"/.idx/dev.nix
+    mkdir -p "$WS_NAME"
+    mkdir "$WS_NAME/.idx/"
+    chmod -R +w "$WS_NAME"
+    cp ${./dev.nix} "$WS_NAME"/.idx/dev.nix
+    mv "$WS_NAME" "$out"
+    echo "flutter app $HOME/myapp"
+    mkdir -p "$out/.idx"
     chmod -R u+w "$out"
-
   '';
 }
